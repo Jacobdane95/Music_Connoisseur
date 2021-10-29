cfc44e13ae8a9c7af8610f0f640d83a873194ec5
Jacobdane95 <81712958+Jacobdane95@users.noreply.github.com>
2021-10-29 18:56:13 -0500

    Add files via upload

---
+++ java/com/app/music_connoisseur/PlayerActivity.kt
@@ -0,0 +1,88 @@
+package com.app.music_connoisseur
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_player.*
+
+
+
+
+class PlayerActivity : AppCompatActivity()  {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_player)
+        setupViews()
+        setupListeners()
+    }
+
+    private fun setupViews () {
+        SpotifyService.getCurrentTrackImage {
+            trackImageView.setImageBitmap(it)
+        }
+
+        SpotifyService.playingState {
+            when(it) {
+                PlayingState.PLAYING -> showPauseButton()
+                PlayingState.STOPPED -> showPlayButton()
+                PlayingState.PAUSED -> showResumeButton()
+            }
+        }
+    }
+
+    private fun setupListeners() {
+        playButton.setOnClickListener {
+            SpotifyService.play("https://open.spotify.com/playlist/6OKRtrnJov6VkTh9nNKMCf?si=QTOISGEuSYK5EH8FuPs76w&utm_source=native-share-menu")
+            showPauseButton()
+        }
+
+        pauseButton.setOnClickListener {
+            SpotifyService.pause()
+            showResumeButton()
+        }
+
+        resumeButton.setOnClickListener {
+            SpotifyService.resume()
+            showPauseButton()
+        }
+        previousButton.setOnClickListener {
+            SpotifyService.previous()
+        }
+        nextButton.setOnClickListener {
+            SpotifyService.next()
+        }
+
+        SpotifyService.subscribeToChanges {
+            SpotifyService.getImage(it.imageUri){
+                trackImageView.setImageBitmap(it)
+            }
+        }
+    }
+
+    private fun InitializeSeekBar() {
+    }
+
+    private fun showPlayButton() {
+        playButton.visibility = View.VISIBLE
+        pauseButton.visibility = View.GONE
+        resumeButton.visibility = View.GONE
+    }
+
+    private fun showPauseButton() {
+        playButton.visibility = View.GONE
+        pauseButton.visibility = View.VISIBLE
+        resumeButton.visibility = View.GONE
+    }
+
+    private fun showResumeButton() {
+        playButton.visibility = View.GONE
+        pauseButton.visibility = View.GONE
+        resumeButton.visibility = View.VISIBLE
+    }
+
+    override fun onStop() {
+        super.onStop()
+        SpotifyService.disconnect()
+    }
+}
